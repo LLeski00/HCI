2c9c2aa908c7b9b2da953e139e8a88e51838797d
mmaras02 <mmaras02@fesb.hr>
2024-12-10 23:05:37 +0100

    fixing for vercel

--- semester-project/src/app/page.tsx
+++ semester-project/src/app/page.tsx
@@ -19,11 +19,11 @@ export default function Home() {
                 <div className="text-content">
                     <h2>Everything you need to know in one place</h2>
                     <p>
-                        Embark on your next snowy adventure with ease! Discover Europe's
-                        top ski resorts, compare features, and plan your dream trip—all in
+                        Embark on your next snowy adventure with ease! Discover top ski resorts in Europe,
+                        compare features, and plan your dream trip—all in
                         one place. From family-friendly slopes to thrilling alpine runs, 
-                        we've got everything you need to make your ski vacation unforgettable. 
-                        Start exploring today!"
+                        we have everything you need to make your ski vacation unforgettable. 
+                        Start exploring today!
                     </p>
                    <Button text="EXPLORE" href="/" />
                 </div>
