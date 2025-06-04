5b5df0ad1b8558a85051863cb640459169be172e
Luka <llesko00@fesb.hr>
2025-06-04 16:14:41 +0200

    Error handle hero

--- semester-project/src/components/hero/hero.tsx
+++ semester-project/src/components/hero/hero.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Navbar } from '../navbar/navbar';
+import React from "react";
+import { Navbar } from "../navbar/navbar";
 import styles from "./hero.module.css";
-import TearEffect from '../tearEffect/tearEffect';
+import TearEffect from "../tearEffect/tearEffect";
 
 interface HeroSectionProps {
     titleTop: string;
@@ -10,23 +10,34 @@ interface HeroSectionProps {
     backgroundImage?: string;
 }
 
-const HeroSection: React.FC<HeroSectionProps> = ({ titleTop, titleBottom, description, backgroundImage }) => {
+const HeroSection: React.FC<HeroSectionProps> = ({
+    titleTop,
+    titleBottom,
+    description,
+    backgroundImage,
+}) => {
     return (
         <>
-            <section className={styles.heroSection}
-                    style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <section
+                className={styles.heroSection}
+                style={{
+                    backgroundImage: backgroundImage
+                        ? `url(${backgroundImage})`
+                        : undefined,
+                }}
+            >
                 <div className={styles.heroOverlay}></div>
                 <Navbar />
                 <div className={styles.heroText}>
                     <h1 className={styles.heroHeading}>{titleTop}</h1>
-                    <h1 className={` ${styles.heroHeading} ${styles.bottom}`}>{titleBottom}</h1>
+                    <h1 className={` ${styles.heroHeading} ${styles.bottom}`}>
+                        {titleBottom}
+                    </h1>
                     <p>{description}</p>
                 </div>
-               
             </section>
             <TearEffect isHero={true} />
         </>
-        
     );
 };
 
