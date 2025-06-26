85863f07add0b715ee981b918cdd8033871314fd
Luka <llesko00@fesb.hr>
2025-06-26 11:36:27 +0200

    Add blogs page

---
+++ semester-project/src/app/blogs/page.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "@/components/navbar/navbar";
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Blogs</h1>
+        </div>
+    );
+}
