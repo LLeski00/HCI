db1609546782fe957f01ea8d1f082e752efb5370
Luka <llesko00@fesb.hr>
2025-07-24 13:43:48 +0200

    Add loading for destination page

---
+++ semester-project/src/app/destinations/[id]/_components/Destination.tsx
@@ -0,0 +1,111 @@
+import { Metadata } from "next";
+import HeroSection from "@/components/hero/hero";
+import { FaPersonSkiing, FaMountain } from "react-icons/fa6";
+import { TbAerialLift } from "react-icons/tb";
+import { GiTwoCoins } from "react-icons/gi";
+import "./resort.css";
+import TearEffect from "@/components/tearEffect/tearEffect";
+import Carousel from "@/components/carousel/carousel";
+import { getResortById } from "@/api/resort";
+import Reviews from "./Reviews";
+import { Suspense } from "react";
+import Loading from "@/components/loading/Loading";
+export const metadata: Metadata = {
+    title: "Destination",
+};
+
+type DestinationProps = {
+    params: { id: string; name: string };
+};
+
+export default async function Destination({ params }: DestinationProps) {
+    const destination = await getResortById(params.id);
+    if (!destination) {
+        return <div>Destination not found</div>;
+    }
+
+    return (
+        <>
+            <HeroSection
+                titleTop=""
+                titleBottom={destination.name || ""}
+                description={destination.country || ""}
+                backgroundImage={destination.images?.[0] || "/images/2.jpg"}
+            />
+
+            <main>
+                <div className="info-description">
+                    <p>{destination.description || ""}</p>
+                </div>
+
+                <TearEffect darkBackground={true} />
+                <div className="info-container">
+                    <div className="info-content">
+                        <div className="info-item">
+                            <FaMountain />
+                            <h3>Elevation</h3>
+                            <div className="item-content">
+                                <p>
+                                    {destination.elevation ||
+                                        "No elevation info"}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="info-item">
+                            <TbAerialLift className="lift" />
+                            <h3>Ski lifts</h3>
+                            <div className="item-content">
+                                <p>Total lifts: {destination.skiLift || 0}</p>
+                            </div>
+                        </div>
+                        <div className="info-item">
+                            <FaPersonSkiing />
+                            <h3>Slope length</h3>
+                            <div className="item-content table">
+                                <p>
+                                    easy slopes: {destination.easySlopes || 0}{" "}
+                                    km
+                                </p>
+                                <p>
+                                    intermediate slopes:{" "}
+                                    {destination.intermediateSlopes || 0} km
+                                </p>
+                                <p>
+                                    difficult slopes:{" "}
+                                    {destination.difficultSlopes || 0} km
+                                </p>
+                            </div>
+                        </div>
+                        <div className="info-item">
+                            <GiTwoCoins />
+                            <h3>Price</h3>
+                            <div className="item-content table">
+                                <p>
+                                    Adults:{" "}
+                                    {destination.adultPrice ||
+                                        "Price not available"}{" "}
+                                    €
+                                </p>
+                                <p>
+                                    Children:{" "}
+                                    {destination.youthPrice ||
+                                        "Price not available"}{" "}
+                                    €
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <TearEffect />
+                </div>
+                <div className="images-content">
+                    <h2>Resort images</h2>
+                    <Carousel destination={destination} />
+                </div>
+                <TearEffect darkBackground={true} />
+                <Suspense fallback={<Loading />}>
+                    <Reviews resort={destination} />
+                </Suspense>
+            </main>
+        </>
+    );
+}
