9125e11ee32b36ce5be25dd5efe90bccc472fefe
Luka <llesko00@fesb.hr>
2025-07-25 12:23:47 +0200

    Error handle auth

--- semester-project/src/api/user.ts
+++ semester-project/src/api/user.ts
@@ -6,19 +6,23 @@ import { User } from "@/types/user";
 import { eq } from "drizzle-orm";
 
 export async function getUserById(userId: string): Promise<User | null> {
+    try {
         const dbData = await db
-                .select()
-                .from(users)
-                .where(eq(users.id, userId));
+            .select()
+            .from(users)
+            .where(eq(users.id, userId));
 
         if (dbData.length === 0) return null;
 
         const user: User = {
-                id: dbData[0].id,
-                email: dbData[0].email,
-                name: dbData[0].name,
-                profile_image: dbData[0].profile_image,
+            id: dbData[0].id,
+            email: dbData[0].email,
+            name: dbData[0].name,
+            profile_image: dbData[0].profile_image,
         };
 
         return user;
+    } catch (error) {
+        throw new Error("Error fetching user by ID: " + error);
+    }
 }
