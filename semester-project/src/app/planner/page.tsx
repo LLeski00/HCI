e8718903c682aef875a281d677d8426194bdbbdd
mmaras02 <mmaras02@fesb.hr>
2025-08-01 11:20:28 +0200

    scroll into view

--- semester-project/src/app/planner/page.tsx
+++ semester-project/src/app/planner/page.tsx
@@ -5,9 +5,19 @@ import PlannerForm from "./_components/planner-form/PlannerForm";
 import PlannerResults from "./_components/planner-results/PlannerResults";
 import { usePlanner } from "@/hooks/usePlanner";
 import Loading from "@/components/loading/Loading";
+import { useEffect, useRef } from "react";
 
 export default function Planner() {
     const { formData, setFormData, results, loading, error } = usePlanner();
+    const resultsRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (results && !loading && resultsRef.current) {
+            resultsRef?.current?.scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+    }, [results]);
 
     return (
         <div>
@@ -17,17 +27,19 @@ export default function Planner() {
                 titleTop=""
                 backgroundImage="/images/2.jpg"
             />
-            {formData ? (
+            <PlannerForm setFormData={setFormData} />
+            {formData && (
                 loading ? (
                     <Loading />
                 ) : error ? (
                     <p style={{ color: "red" }}>{error}</p>
                 ) : (
-                    results && <PlannerResults results={results} />
+                    results && (
+                        <div ref={resultsRef}>
+                            <PlannerResults results={results} />
+                        </div>
+                    )
                 )
-            ) : (
-
-                <PlannerForm setFormData={setFormData} />
             )}
         </div>
     );
