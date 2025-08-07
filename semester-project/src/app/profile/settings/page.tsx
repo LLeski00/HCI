9a50012319199dedbc6e4ab2739d71d0c9bfb40a
mmaras02 <mmaras02@fesb.hr>
2025-08-07 23:02:09 +0200

    added user logout and removed settings option

--- semester-project/src/app/profile/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import Favourites from "../_components/favourites/Favourites";
-
-export default function SettingsPage() {
-    return (
-        <div>
-            <h2>Settings</h2>
-        </div>
-    );
-}
