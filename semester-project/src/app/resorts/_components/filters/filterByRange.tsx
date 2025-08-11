5b0755888849ec2e2fc11e4b7f5b56f6db722929
mmaras02 <mmaras02@fesb.hr>
2025-08-11 22:41:42 +0200

    filters by range fixed option for remove km and navbar center

--- semester-project/src/app/resorts/_components/filters/filterByRange.tsx
+++ semester-project/src/app/resorts/_components/filters/filterByRange.tsx
@@ -1,9 +1,17 @@
 import { FilterDataProps } from "../../types/filter";
 import "./filters.css";
 
-const distanceRange = ["0-20", "20-50", "50-80", "80-150", "150-600", "remove"];
+const distanceRange = ["0-20", "20-50", "50-80", "80-150", "150-600"];
 
 export default function FilterByRange({ filterData, setFilterData }: FilterDataProps) {
+
+    const handleClick = (value: string) => {
+        if (filterData.rangeFilter === value) {
+            setFilterData({ ...filterData, rangeFilter: "" });
+        } else {
+            setFilterData({ ...filterData, rangeFilter: value });
+        }
+    };
 
     return (
         <>
@@ -11,11 +19,10 @@ export default function FilterByRange({ filterData, setFilterData }: FilterDataP
                 <h4>Filter by ski slope length</h4>
                 <div className="filter-content">
                     {distanceRange.map((range) => (
-                        <label key={range} className="button-item">
+                        <label key={range} className="button-item" onClick={() => handleClick(range)}>
                             <input type="radio"
                                 value={range}
                                 checked={filterData.rangeFilter === range}
-                                onChange={(e) => setFilterData({ ...filterData, rangeFilter: e.target.value })}
                             />
                             {range} km
                         </label>
