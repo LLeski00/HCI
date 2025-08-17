fad6981c06be5e563e6bd6e206eb73ae0b05ba40
Luka <llesko00@fesb.hr>
2025-08-17 16:18:00 +0200

    Display message on empty resorts and disable button on form loading

--- semester-project/src/app/planner/_components/planner-form/PlannerForm.tsx
+++ semester-project/src/app/planner/_components/planner-form/PlannerForm.tsx
@@ -1,118 +1,121 @@
-"use client";
+'use client';
 
-import { getCoordinates } from "@/app/api/coordinate";
-import { Coordinates } from "@/types/coordinate";
-import { PlannerFormData } from "@/types/planner";
-import { FC, useState } from "react";
-import { Country, getData } from "country-list";
+import { getCoordinates } from '@/app/api/coordinate';
+import { Coordinates } from '@/types/coordinate';
+import { PlannerFormData } from '@/types/planner';
+import { FC, useState } from 'react';
+import { Country, getData } from 'country-list';
 import styles from './planner-form.module.css';
-import { Button } from "@/components/button/Button";
+import { Button } from '@/components/button/Button';
 
 interface PlannerFormProps {
-    setFormData: Function;
+  setFormData: Function;
 }
 
 const PlannerForm: FC<PlannerFormProps> = ({ setFormData }) => {
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-    const today = new Date().toISOString().split("T")[0];
-    const countries: Country[] = getData();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const today = new Date().toISOString().split('T')[0];
+  const countries: Country[] = getData();
 
-    function isFormValid(form: HTMLFormElement): boolean {
-        const startDate = new Date(form.startDate.value);
-        const endDate = new Date(form.endDate.value);
+  function isFormValid(form: HTMLFormElement): boolean {
+    const startDate = new Date(form.startDate.value);
+    const endDate = new Date(form.endDate.value);
 
-        if (endDate < startDate) {
-            setErrorMessage("End date cannot be before start date.");
-            return false;
-        }
-
-        setErrorMessage(null);
-        return true;
+    if (endDate < startDate) {
+      setErrorMessage('End date cannot be before start date.');
+      return false;
     }
 
-    async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const form: HTMLFormElement = event.currentTarget;
+    setErrorMessage(null);
+    return true;
+  }
 
-        if (!isFormValid(form)) return;
+  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form: HTMLFormElement = event.currentTarget;
 
-        const currentLocation = form.city.value + ", " + form.country.value;
-        const coordinates: Coordinates | null = await getCoordinates(
-            currentLocation
-        );
+    if (!isFormValid(form)) return;
 
-        if (!coordinates) {
-            setErrorMessage("Invalid current location. Please try again.");
-            return;
-        }
+    setIsSubmitting(true);
 
-        const formData: PlannerFormData = {
-            startDate: new Date(form.startDate.value),
-            endDate: new Date(form.endDate.value),
-            numOfPeople: parseInt(form.numOfPeople.value),
-            budget: parseFloat(form.budget.value),
-            currentLocation: coordinates,
-        };
-        setFormData(formData);
-    }
-
-    return (
-        <form onSubmit={handleFormSubmit}
-            className={styles.form}>
-
-            <div className={styles.gridForm}>
-
-                <div className={styles.gridItem}>
-                    <label> Start date: </label>
-                    <input type="date" name="startDate" min={today} required />
-                </div>
-
-                <div className={styles.gridItem}>
-                    <label> End date: </label>
-                    <input type="date" name="endDate" min={today} required />
-                </div>
-
-                <div className={styles.gridItem}>
-                    <label> Number of people: </label>
-                    <input type="number" name="numOfPeople" min={1} required />
-                </div>
-
-                <div className={styles.gridItem}>
-                    <label> Budget: </label>
-                    <input type="number" name="budget" min={10} required />
-
-                </div>
-
-                <div className={styles.gridItem}>
-                    <label>Your Country: </label>
-                    <select name="country" required>
-                        <option value="">Select starting country</option>
-                        {countries.map((country) => (
-                            <option key={country.code} value={country.name}>
-                                {country.name}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-
-                <div className={styles.gridItem}>
-                    <label>Your City: </label>
-                    <input
-                        type="text"
-                        name="city"
-                        placeholder="Enter your starting city"
-                        required
-                    />
-                </div>
-
-            </div>
-            <div className={styles.buttonContainer}>
-                <Button text="Find resorts" type="submit" />
-                {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-            </div>
-        </form>
-
+    const currentLocation = form.city.value + ', ' + form.country.value;
+    const coordinates: Coordinates | null = await getCoordinates(
+      currentLocation
     );
+
+    if (!coordinates) {
+      setErrorMessage('Invalid current location. Please try again.');
+      setIsSubmitting(false);
+      return;
+    }
+
+    const formData: PlannerFormData = {
+      startDate: new Date(form.startDate.value),
+      endDate: new Date(form.endDate.value),
+      numOfPeople: parseInt(form.numOfPeople.value),
+      budget: parseFloat(form.budget.value),
+      currentLocation: coordinates,
+    };
+    setFormData(formData);
+    setIsSubmitting(false);
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit} className={styles.form}>
+      <div className={styles.gridForm}>
+        <div className={styles.gridItem}>
+          <label> Start date: </label>
+          <input type="date" name="startDate" min={today} required />
+        </div>
+
+        <div className={styles.gridItem}>
+          <label> End date: </label>
+          <input type="date" name="endDate" min={today} required />
+        </div>
+
+        <div className={styles.gridItem}>
+          <label> Number of people: </label>
+          <input type="number" name="numOfPeople" min={1} required />
+        </div>
+
+        <div className={styles.gridItem}>
+          <label> Budget: </label>
+          <input type="number" name="budget" min={10} required />
+        </div>
+
+        <div className={styles.gridItem}>
+          <label>Your Country: </label>
+          <select name="country" required>
+            <option value="">Select starting country</option>
+            {countries.map((country) => (
+              <option key={country.code} value={country.name}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className={styles.gridItem}>
+          <label>Your City: </label>
+          <input
+            type="text"
+            name="city"
+            placeholder="Enter your starting city"
+            required
+          />
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <Button
+          text={isSubmitting ? 'Finding resorts...' : 'Find resorts'}
+          type="submit"
+          disabled={isSubmitting}
+        />
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      </div>
+    </form>
+  );
 };
 
 export default PlannerForm;
--- semester-project/src/app/resorts/_components/SkiResortList.tsx
+++ semester-project/src/app/resorts/_components/SkiResortList.tsx
@@ -1,126 +1,134 @@
 //import Pagination from "./pagination";
-"use client";
-import ResortCard from "../../../components/resort-card/ResortCard";
-import { FilterProps } from "../types/filter";
-import { getTotalDistance, parseDistanceRange } from "@/utils/getDistance";
-import Pagination from "./pagination/pagination";
-import { useEffect, useMemo, useState } from "react";
-import { ResortInfo } from "@/types/resort";
-import { User } from "@/types/user";
+'use client';
+import ResortCard from '../../../components/resort-card/ResortCard';
+import { FilterProps } from '../types/filter';
+import { getTotalDistance, parseDistanceRange } from '@/utils/getDistance';
+import Pagination from './pagination/pagination';
+import { useEffect, useMemo, useState } from 'react';
+import { ResortInfo } from '@/types/resort';
+import { User } from '@/types/user';
 
 type ResortsListProps = {
-    destinations: ResortInfo[];
-    filterData: FilterProps;
-    user: User | null;
-    favouriteIds: string[] | null;
+  destinations: ResortInfo[];
+  filterData: FilterProps;
+  user: User | null;
+  favouriteIds: string[] | null;
 };
 
 const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 20;
 
 export default function SkiResortsList({
-    destinations,
-    filterData,
-    user,
-    favouriteIds,
+  destinations,
+  filterData,
+  user,
+  favouriteIds,
 }: ResortsListProps) {
-    const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const filteredResorts = useMemo(() => {
-        let resortsToDisplay = [...destinations];
+  const filteredResorts = useMemo(() => {
+    let resortsToDisplay = [...destinations];
 
-        if (filterData.resortFilter) {
-            resortsToDisplay = resortsToDisplay.filter((resort) =>
-                resort.name.toLowerCase().includes(filterData.resortFilter.toLowerCase())
-            );
-        }
+    if (filterData.resortFilter) {
+      resortsToDisplay = resortsToDisplay.filter((resort) =>
+        resort.name
+          .toLowerCase()
+          .includes(filterData.resortFilter.toLowerCase())
+      );
+    }
 
-        if (filterData.countryFilter.length > 0) {
-            resortsToDisplay = resortsToDisplay.filter((resort) =>
-                filterData.countryFilter.includes(resort.country)
-            );
-        }
+    if (filterData.countryFilter.length > 0) {
+      resortsToDisplay = resortsToDisplay.filter((resort) =>
+        filterData.countryFilter.includes(resort.country)
+      );
+    }
 
-        if (filterData.rangeFilter && filterData.rangeFilter !== "remove") {
-            resortsToDisplay = resortsToDisplay.filter((resort) => {
-                const totalSlopeDistance = getTotalDistance(
-                    resort.easySlopes ?? 0,
-                    resort.intermediateSlopes ?? 0,
-                    resort.difficultSlopes ?? 0
-                );
-                const { min, max } = parseDistanceRange(filterData.rangeFilter);
-                return totalSlopeDistance >= min && totalSlopeDistance <= max;
-            });
-        }
+    if (filterData.rangeFilter && filterData.rangeFilter !== 'remove') {
+      resortsToDisplay = resortsToDisplay.filter((resort) => {
+        const totalSlopeDistance = getTotalDistance(
+          resort.easySlopes ?? 0,
+          resort.intermediateSlopes ?? 0,
+          resort.difficultSlopes ?? 0
+        );
+        const { min, max } = parseDistanceRange(filterData.rangeFilter);
+        return totalSlopeDistance >= min && totalSlopeDistance <= max;
+      });
+    }
 
-        switch (filterData.sortFilter) {
-            case "price-asc":
-                resortsToDisplay.sort((a, b) => Number(a.adultPrice) - Number(b.adultPrice));
-                break;
-            case "price-desc":
-                resortsToDisplay.sort((a, b) => Number(b.adultPrice) - Number(a.adultPrice));
-                break;
-            case "rating-asc":
-                resortsToDisplay.sort((a, b) => Number(a.review) - Number(b.review));
-                break;
-            case "rating-desc":
-                resortsToDisplay.sort((a, b) => Number(b.review) - Number(a.review));
-                break;
-            default:
-                break;
-        }
+    switch (filterData.sortFilter) {
+      case 'price-asc':
+        resortsToDisplay.sort(
+          (a, b) => Number(a.adultPrice) - Number(b.adultPrice)
+        );
+        break;
+      case 'price-desc':
+        resortsToDisplay.sort(
+          (a, b) => Number(b.adultPrice) - Number(a.adultPrice)
+        );
+        break;
+      case 'rating-asc':
+        resortsToDisplay.sort((a, b) => Number(a.review) - Number(b.review));
+        break;
+      case 'rating-desc':
+        resortsToDisplay.sort((a, b) => Number(b.review) - Number(a.review));
+        break;
+      default:
+        break;
+    }
 
-        return resortsToDisplay;
-    }, [destinations, filterData]);
+    return resortsToDisplay;
+  }, [destinations, filterData]);
 
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-        document.getElementById("ski-resorts")?.scrollIntoView({
-            behavior: "smooth",
-        });
-    };
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    document.getElementById('ski-resorts')?.scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
 
-    useEffect(() => {
-        const params = new URLSearchParams();
-        if (currentPage !== 1) params.set("page", currentPage.toString());
+  useEffect(() => {
+    const params = new URLSearchParams();
+    if (currentPage !== 1) params.set('page', currentPage.toString());
 
-        const newUrl = params.toString()
-            ? `/resorts?${params.toString()}`
-            : "/resorts";
-        window.history.pushState({}, "", newUrl);
-    }, [filterData, currentPage]);
+    const newUrl = params.toString()
+      ? `/resorts?${params.toString()}`
+      : '/resorts';
+    window.history.pushState({}, '', newUrl);
+  }, [filterData, currentPage]);
 
-    useEffect(() => setCurrentPage(1), [filterData]);
+  useEffect(() => setCurrentPage(1), [filterData]);
 
-    //const filteredResorts = filterResorts();
-    const pagesCount = Math.ceil(filteredResorts.length / PAGE_SIZE);
+  //const filteredResorts = filterResorts();
+  const pagesCount = Math.ceil(filteredResorts.length / PAGE_SIZE);
 
-    const startIndex = (currentPage - 1) * PAGE_SIZE;
-    const endIndex = startIndex + PAGE_SIZE;
-    const resortsToDisplay = filteredResorts.slice(startIndex, endIndex);
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const resortsToDisplay = filteredResorts.slice(startIndex, endIndex);
 
-    return (
+  return (
+    <>
+      {resortsToDisplay && resortsToDisplay.length > 0 ? (
         <>
-            <ul className="ski-resorts-list"
-                id="ski-resorts">
-                {resortsToDisplay &&
-                    resortsToDisplay.map((resort) => (
-                        <ResortCard
-                            key={resort.id}
-                            resort={resort}
-                            user={user}
-                            isFavourite={
-                                favouriteIds?.includes(resort.id) ?? false
-                            }
-                        />
-                    ))}
-            </ul>
-            <div className="pagination">
-                <Pagination
-                    currentPage={currentPage}
-                    totalPages={pagesCount}
-                    onPageChange={handlePageChange}
-                />
-            </div>
+          <ul className="ski-resorts-list" id="ski-resorts">
+            {resortsToDisplay.map((resort) => (
+              <ResortCard
+                key={resort.id}
+                resort={resort}
+                user={user}
+                isFavourite={favouriteIds?.includes(resort.id) ?? false}
+              />
+            ))}
+          </ul>
+          <div className="pagination">
+            <Pagination
+              currentPage={currentPage}
+              totalPages={pagesCount}
+              onPageChange={handlePageChange}
+            />
+          </div>
         </>
-    );
+      ) : (
+        <p className="no-resorts">No resorts found.</p>
+      )}
+    </>
+  );
 }
